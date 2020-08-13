0625a8b32a76f611dfd0f71c9839923bcae55c15
yokra9 <53964890+yokra9@users.noreply.github.com>
2020-08-13 19:49:32 +0900

    solve memory consumption issue attributable to background job

--- RunCatPS/src/runcat.ps1
+++ RunCatPS/src/runcat.ps1
@@ -1,16 +1,13 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
-# カレントディレクトリを取得
-$path = Split-Path -Parent $MyInvocation.MyCommand.Path
-
 # ウィンドウを非表示化
 $cscode = @"
-// Win32Api を読み込むための C# コード
-[DllImport("user32.dll")]
-[return: MarshalAs(UnmanagedType.Bool)]
-// ウィンドウの状態を制御するため ShowWindowAsync() を extern する
-public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+    // Win32Api を読み込むための C# コード
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    // ウィンドウの状態を制御するため ShowWindowAsync() を extern する
+    public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
 "@
 $Win32Functions = Add-Type -MemberDefinition $cscode -name Win32ShowWindowAsync -namespace Win32Functions -PassThru
 $Win32Functions::ShowWindowAsync((Get-Process -PID $pid).MainWindowHandle, 0) > $null # bool 値を返すので null に捨てる
@@ -26,6 +23,7 @@ if ((Get-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Wind
 }
 
 # リソースの読みこみ
+$path = Split-Path -Parent $MyInvocation.MyCommand.Path
 $cats = @(
     New-Object System.Drawing.Icon -ArgumentList "$path\\resources\\${theme}_cat0.ico";
     New-Object System.Drawing.Icon -ArgumentList "$path\\resources\\${theme}_cat1.ico";
@@ -34,30 +32,36 @@ $cats = @(
     New-Object System.Drawing.Icon -ArgumentList "$path\\resources\\${theme}_cat4.ico"
 )
 
+# CPU 負荷の取得が GUI プロセスをブロックしないよう、バックグラウンドジョブとして実行する
+$job = Start-Job -ScriptBlock {
+    Get-Counter -Counter "\Processor(_Total)\% Processor Time" -Continuous | ForEach-Object { 
+        $_.CounterSamples.CookedValue 
+    } 
+}
+
 # CPU使用率を定期的に取得するため、タイマーオブジェクトを作成する
 $cpuTimer = New-Object Windows.Forms.Timer
 
 # タイマーのイベントハンドラからも書き込みたい変数を script スコープで宣言
-$script:jobId = $null
-$script:idx = 0 
-$script:cpuUsage = 0.0
+$script:cpuUsage = 1
 
 $cpuTimer.Add_Tick( {
         $cpuTimer.Stop()
 
-        # CPU 負荷の取得が GUI プロセスをブロックしないよう、バックグラウンドジョブとして処理を行う
-        $script:jobId = (Start-Job -ScriptBlock {
-                [double]((Get-Counter -Counter  "\Processor(_Total)\% Processor Time").CounterSamples).CookedValue 
-            }).Id 
+        # バックグラウンドジョブから結果を取得する
+        $script:cpuUsage = [double](Receive-Job $job)[0]
 
         $cpuTimer.Start()
     })
-  
+
 $cpuTimer.Interval = 3000
 $cpuTimer.Start()
 
 # タスクトレイアイコンを任意のタイミングで差し替えるため、タイマーオブジェクトを作成する
 $animateTimer = New-Object Windows.Forms.Timer
+
+# タイマーのイベントハンドラからも書き込みたい変数を script スコープで宣言
+$script:idx = 0 
 
 $animateTimer.Add_Tick( {
         $animateTimer.Stop()
@@ -67,16 +71,9 @@ $animateTimer.Add_Tick( {
         if ($script:idx -eq 5) { $script:idx = 0 }
 
         # CPU 使用率をバックグラウンド処理結果から取得
-        if ($script:jobId -ne $null) {
-            $u = (Receive-Job -Id $script:jobId)
-            # CPU 使用率に変化があったとき
-            if ($u -ne $null) { 
-                $script:cpuUsage = $u 
-                $notifyIcon.Text = $script:cpuUsage
-                # ネコチャンの速さを変更
-                $animateTimer.Interval = (200.0 / [System.Math]::Max(1.0, [System.Math]::Min(20.0, $script:cpuUsage / 5)))
-            }           
-        }
+        $notifyIcon.Text = $script:cpuUsage
+        # ネコチャンの速さを変更
+        $animateTimer.Interval = (200.0 / [System.Math]::Max(1.0, [System.Math]::Min(20.0, $script:cpuUsage / 5)))
 
         $animateTimer.Start()
     })
@@ -96,6 +93,7 @@ $notifyIcon.add_Click( {
 # アイコンを押すまで終わらないよう、メッセージループを回す
 [System.Windows.Forms.Application]::Run($applicationContext)
 
+# 終了処理
 $cpuTimer.Stop()
 $animateTimer.Stop()
 $notifyIcon.Visible = $false
